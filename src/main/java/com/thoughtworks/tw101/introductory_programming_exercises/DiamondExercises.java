071b2cb2bd8343b818c37ef1e326e756c6a1c6df
administrator <administrator@c02rx2e5fvh6.local>
2017-02-15 18:57:06 -0500

    fixed char encoding issues in the diamond exercises file

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+/**
+ * Created by administrator on 2/15/17.
+ */
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
@@ -7,96 +10,76 @@ public class DiamondExercises {
         drawADiamondWithYourName(3);
     }
 
-    //    Isosceles Triangle
-//    Given a number n, print a centered triangle. Example for n=3:
-//              *
-//             ***
-//            *****
-    private static void drawAnIsoscelesTriangle(int n) { 
-        int rowLength = (n * 2) - 1;  
+    private static void drawAnIsoscelesTriangle(int n){
+        int rowLength = (n*2)-1;
 
-        for (int rowNumber = 0; rowNumber < n; rowNumber++) { 
-            int numberOfStarsInRow = (rowNumber * 2) + 1; 
-            System.out.println(buildRow(numberOfStarsInRow, rowLength)); 
-        } 
-    }
-
-    //    Diamond
-//    Given a number n, print a centered diamond. Example for n=3:
-//              *
-//             ***
-//            *****
-//             ***
-//              *
-    private static void drawADiamond(int n) {
-         int totalNumberOfRows = (n * 2) - 1;
-         int rowLength = (n * 2) - 1; 
-        int numberOfStarsInRow;
-
-          for (int rowNumber = 0; rowNumber < totalNumberOfRows; rowNumber++) { 
-            if (rowNumber < totalNumberOfRows / 2 + 1) {
-                 numberOfStarsInRow = (rowNumber * 2) + 1;
-             } else { 
-                numberOfStarsInRow = (totalNumberOfRows - rowNumber) * 2 - 1; 
-            } 
+        for (int rowNumber = 0; rowNumber < n; rowNumber++) {
+            int numberOfStarsInRow = (rowNumber*2)+1;
             System.out.println(buildRow(numberOfStarsInRow, rowLength));
         }
-     }
+    }
 
-//    Diamond with Name
-//    Given a number n, print a centered diamond with your name in place of the middle line. Example for n=3:
-//
-//             *
-//            ***
-//           Bill
-//            ***
-//             *
-// Note - name is not being centered 
-    private static void drawADiamondWithYourName(int n) { 
-        int totalNumberOfRows = (n * 2) - 1;
-         int rowLength = (n * 2) - 1;  
+    private static void drawADiamond(int n){
+        int totalNumberOfRows = (n*2)-1;
+        int rowLength = (n*2)-1;
+        int numberOfStarsInRow;
 
-        for (int rowNumber = 0; rowNumber < totalNumberOfRows; rowNumber++) { 
-            if (rowNumber < totalNumberOfRows / 2) {
-                 System.out.println(buildRow((rowNumber * 2) + 1, rowLength));
+        for (int rowNumber = 0; rowNumber < totalNumberOfRows; rowNumber++){
+            if (rowNumber < totalNumberOfRows / 2 + 1) {
+                numberOfStarsInRow = (rowNumber * 2) + 1;
             }
-            else if (rowNumber == totalNumberOfRows / 2) { 
+            else {
+                numberOfStarsInRow = (totalNumberOfRows - rowNumber) * 2 - 1;
+            }
+        System.out.println(buildRow(numberOfStarsInRow, rowLength));
+        }
+    }
+
+    private static void drawADiamondWithYourName(int n){
+        int totalNumberOfRows = (n*2)-1;
+        int rowLength = (n*2)-1;
+
+        for (int rowNumber = 0; rowNumber < totalNumberOfRows; rowNumber++){
+            if (rowNumber < totalNumberOfRows / 2){
+                System.out.println(buildRow((rowNumber*2)+1, rowLength));
+            }
+            else if (rowNumber == totalNumberOfRows / 2){
                 System.out.println("Allie");
             }
             else {
-             System.out.println(buildRow((totalNumberOfRows - rowNumber) * 2 - 1, rowLength));
-            }     
+                System.out.println(buildRow((totalNumberOfRows - rowNumber) * 2 - 1, rowLength));
+            }
         }
-    }  
+    }
 
-    private static String buildSpaces(int numberOfSpacesInRow) {
-         String spaces = "";  
-
-        for (int i = 0; i < numberOfSpacesInRow; i++) {
-             spaces += " "; 
-        }
-         return spaces;
-     }  
-
-    private static String buildStars(int numberOfStarsInRow) {
-         String stars = "";  
-        for (int i = 0; i < numberOfStarsInRow; i++) {
-             stars += "*";
-        }
-         return stars; 
-    }  
-
-    private static String buildRow(int numberOfStarsInRow, int rowLength) { 
+    private static String buildRow(int numberOfStarsInRow, int rowLength) {
         String spaces = "";
-         String stars = ""; 
+        String stars = "";
         String row;
-
-          int numberOfSpacesInRow = (rowLength - numberOfStarsInRow) / 2;  
+        int numberOfSpacesInRow = (rowLength - numberOfStarsInRow) / 2;
 
         spaces += buildSpaces(numberOfSpacesInRow);
-         stars += buildStars(numberOfStarsInRow);  
+        stars += buildStars(numberOfStarsInRow);
 
-        row = spaces + stars + spaces; 
+        row = spaces + stars + spaces;
         return row;
-    } 
+    }
+
+    private static String buildSpaces(int numberOfSpacesInRow){
+        String spaces = "";
+
+        for (int i = 0; i < numberOfSpacesInRow; i++) {
+            spaces += " ";
+        }
+        return spaces;
+    }
+
+    private static String buildStars(int numberOfStarsInRow) {
+        String stars="";
+
+        for (int i = 0; i < numberOfStarsInRow; i++) {
+            stars += "*";
+        }
+        return stars;
+    }
 }
