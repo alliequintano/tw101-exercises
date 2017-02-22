4d27ae478d981702be46fdd50cba8fdc7605c3ce
alexandra quintano <alexandraquintano@C02RX2E5FVH6.netgear.com>
2017-02-21 20:51:18 -0500

    fizzbuzz completed

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -33,5 +33,19 @@ public class FizzBuzz {
 
     private static void fizzBuzz() {
 
+        for (int number = 1; number <= 100; number++) {
+
+            if (number % 15 == 0)
+                System.out.print("FizzBuzz");
+            else if (number % 3 == 0)
+                System.out.print("Fizz");
+            else if (number % 5 == 0)
+                System.out.print("Buzz");
+            else
+                System.out.print(number);
+
+            System.out.println();
+        }
+
     }
 }
